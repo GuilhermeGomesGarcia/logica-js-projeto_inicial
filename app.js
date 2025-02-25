4b56a63d106daf192fe20430d48129ea5711791b
GuilhermeGomesGarcia <guilherneggarcia@gmail.com>
2025-02-25 18:54:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
-alert ("Bem vindo ao jogo de adivinhação!");
+let nomeUsuario = prompt (`Bem vindo ao jogo de adivinhação! Qual o seu nome?`);
+alert (`Olá ${nomeUsuario}! Vamos começar!`);
+
 let numeroSecreto = 32;
 console.log (numeroSecreto);
-let numeroChute = prompt ("Escolha um número entre 0 e 100:");
+let numeroChute;
+let tentativas = 1;
 
-if (numeroChute == numeroSecreto) 
-    {
-    alert (`Parabéns, você acertou, o número secreto é ${numeroSecreto} !`);
-    }
-    else if (numeroChute > numeroSecreto)
-    {
-    alert (`O número secreto é menor que o chute!`);
-    }
-    else if (numeroChute < numeroSecreto)
-    {
-    alert (`O número secreto é maior que o chute!`);	
-    }
+while(numeroChute != numeroSecreto)
+{
     
+
+    numeroChute = prompt (`${nomeUsuario} escolha um número entre 0 e 100:`);
+
+    
+    if (numeroChute == null) 
+    {
+        alert (`${nomeUsuario}, você desistiu do jogo!`);
+        break;
+    }
+        
+    
+    while (numeroChute < 0 || numeroChute > 100) 
+    {
+        alert (`${nomeUsuario}, o número deve ser entre 0 e 100!`);
+
+        numeroChute = prompt (`${nomeUsuario} escolha um número entre 0 e 100:`);
+    }
+
+    if (numeroChute == numeroSecreto) 
+        {
+            alert (`Parabéns, você acertou, o número secreto é ${numeroSecreto}, com ${tentativas} tentavivas !`);
+        }
+                
+        else 
+        {
+            if(numeroChute > numeroSecreto)
+            {
+                alert (`O número secreto é menor que ${numeroChute}!`);
+            }
+            else 
+            {
+                alert (`O número secreto é maior que ${numeroChute}!`);
+            }
+            
+            tentativas++;
+        }
+
+
+}
+
+
+// Fim do código
